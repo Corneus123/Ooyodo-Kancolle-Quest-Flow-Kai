9d55d33624be2f8236425ec3ddbbd766d3a5e485
Corneus <68751278+Corneus123@users.noreply.github.com>
2023-10-17 17:57:25 +0900

    Updated

---
+++ updates_logs.js
@@ -0,0 +1,210 @@
+const NEW_QUESTS = "C37,F89,B139,Bq10,Bq11";
+
+  const updates = {
+  "update_1": {
+    "implementationDate": "2017-09-14T00:00:01+09:00",
+    "questsAdded": ["A80","C12","B105","B106","F61","F62","F63"],
+    "questsRemoved": [],
+    "questsReqModified": {
+      "A79": {
+        "removed": ["Unknown"],
+        "added": ["B39"]
+      },
+      "B104": {
+        "removed": [],
+        "added": ["C2"]
+      }
+    }
+  },
+  "update_2": {
+    "implementationDate": "2017-09-17T23:05:00+09:00",
+    "questsAdded": [],
+    "questsRemoved": [],
+    "questsReqModified": {
+      "F42": {
+        "removed": ["A15"],
+        "added": []
+      }
+    }
+  },
+  "update_3": {
+    "implementationDate": "2017-09-30T17:15:00+09:00",
+    "questsAdded": ["SB01","SB02","SB03"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_18102017": {
+    "implementationDate": "2017-10-21T20:00:00+09:00",
+    "questsAdded": ["B107","D25","D26"],
+    "questsRemoved": ["SB01","SB02","SB03"],
+    "questsReqModified": {}
+  },
+  "update_25102017": {
+    "implementationDate": "2017-10-26T20:00:00+09:00",
+    "questsAdded": ["A81","A82","B108","B109","D27"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_13122017": {
+    "implementationDate": "2017-12-15T01:00:00+09:00",
+    "questsAdded": ["Bq5","B110","F64","F65","F66","F67"],
+    "questsRemoved": [],
+    "questsReqModified": {
+      "B108": {
+        "removed": ["Unknown"],
+        "added": ["B20"]
+      }
+    }
+  },
+  "update_27122017": {
+    "implementationDate": "2018-01-10T00:15:00+09:00",
+    "questsAdded": ["A83","Bq6","F68","C15","SB26","SB27","SB28","SB29"],
+    "questsRemoved": [],
+    "questsReqModified": {
+      "B109": {
+        "removed": [],
+        "added": ["A81"]
+      },
+      "A81": {
+        "removed": [],
+        "added": ["B74"]
+      }
+    }
+  },
+  "update_17012018": {
+    "implementationDate": "2018-01-22T00:55:00+09:00",
+    "questsAdded": ["A84","B111","B112","C16","F69"],
+    "questsRemoved": ["SB26","SB27","SB28","SB29"],
+    "questsReqModified": {}
+  },
+  "update_05022018": {
+    "implementationDate": "2018-02-08T00:40:00+09:00",
+    "questsAdded": ["A85","B113","B114","C17","F70","F71"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_18042018_23h14": {
+    "implementationDate": "2018-04-18T14:14:31.736Z",
+    "questAdded": ["A86","B115","C18","F72"]
+  },
+  "update_27042018_00h20": {
+    "implementationDate": "2018-04-26T15:20:41.103Z",
+    "questAdded": ["5A01","5A02","5A03","5A04","5A05"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_23052018_17h07": {
+    "implementationDate": "2018-05-23T08:07:18.800Z",
+    "questAdded": ["Su01","Su02","Su03","Su04","Su05","Su06","Su07","Su08","Su09"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_07072018_00h56": {
+    "implementationDate": "2018-07-06T15:56:06.841Z",
+    "questAdded": ["A88","B117","B118","B119","C19","C20","C21","D28","F73","F74","F75"],
+    "questsRemoved": ["Su01","Su02","Su03","Su04","Su05","Su06","Su07","Su08","Su09","5A01","5A02","5A03","5A04","5A05"],
+    "questsReqModified": {}
+  },
+  "update_21072018_22h26": {
+    "implementationDate": "2018-07-21T13:26:12.372Z",
+    "questAdded": ["A89","B120","B121","Bq7","C22","F76","F77"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_05082018_18h51": {
+    "implementationDate": "2018-08-05T09:51:30.875Z",
+    "questAdded": ["B122","C22","C23","F78"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_05082018_19h23": {
+    "implementationDate": "2018-08-05T10:23:15.406Z",
+    "questAdded": ["C18"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_28102018_11h31": {
+    "implementationDate": "2018-10-28T02:31:11.599Z",
+    "questAdded": ["A90","B123","B124","C24","F79"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_25112018_13h45": {
+    "implementationDate": "2018-11-25T04:45:46.749Z",
+    "questAdded": ["A91","B125","B126","Bq8"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_12012019_23h51": {
+    "implementationDate": "2019-01-12T14:51:12.171Z",
+    "questAdded": ["F80","F81"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_12012019_23h59": {
+    "implementationDate": "2019-01-12T14:59:01.206Z",
+    "questAdded": ["F80","F81"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_13012019_00h18": {
+    "implementationDate": "2019-01-12T15:18:29.163Z",
+    "questAdded": ["F80","F81","B127","Bq9"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_13012019_00h47": {
+    "implementationDate": "2019-01-12T15:47:47.595Z",
+    "questAdded": ["SB33","SB34","SB35","SB36"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_10022019_00h33": {
+    "implementationDate": "2019-02-09T15:33:04.299Z",
+    "questAdded": ["SB41","SB42","B128"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_17032019_12h09": {
+    "implementationDate": "2019-03-17T03:09:19.728Z",
+    "questAdded": ["A92","B129","B130","Bm8","F82","C29","F83"],
+    "questsRemoved": ["SB33","SB34","SB35","SB36","SB41","SB42"],
+    "questsReqModified": {}
+  },
+  "update_17092019_22h04": {
+    "implementationDate": "2019-09-17T13:04:15.449Z",
+    "questAdded": ["B131","B132","C31","F84","F85","F86","F87"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_17092019_22h53": {
+    "implementationDate": "2019-09-17T13:53:49.444Z",
+    "questAdded": ["B133","B134","D29"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_17092019_23h14": {
+    "implementationDate": "2019-09-17T14:14:54.692Z",
+    "questAdded": ["B135","B136"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_18092019_00h28": {
+    "implementationDate": "2019-09-17T15:28:34.205Z",
+    "questAdded": ["B135","B136","B137","C33","C34","D30","D31","F88"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_18092019_00h41": {
+    "implementationDate": "2019-09-17T15:41:29.966Z",
+    "questAdded": ["B138","C35"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  },
+  "update_18092019_00h50": {
+    "implementationDate": "2019-09-17T15:50:12.951Z",
+    "questAdded": ["C37","F89","B139","Bq10","Bq11"],
+    "questsRemoved": [],
+    "questsReqModified": {}
+  }
+};
